e171bfc22a7e1cccf8e22c00be3cb8802aeae407
edilkk <edilmoldokanov@gmail.com>
2020-02-19 13:11:55 -0600

    ad

--- public_sg.tf
+++ public_sg.tf
@@ -47,14 +47,14 @@ resource "aws_security_group" "private" {
     to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-
+  }
     egress {
   from_port   = 0
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
-}
-  
+
+    }
 tags = {
       Name = "${var.Name}.privateDB"
       Env = "${var.Env}"
