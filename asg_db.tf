f4e5f388f7493bab06ba1462e115e54ee98ac1ad
edilkk <edilmoldokanov@gmail.com>
2020-02-21 13:23:31 -0600

    sa

---
+++ asg_db.tf
@@ -0,0 +1,29 @@
+module "mysql" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "3.0"
+  name = "mysql"
+
+  # Launch configuration
+  lc_name = "mysql-lc"
+
+  image_id        = "${var.ami}"
+  instance_type   = "${var.instance_type}"
+  security_groups = ["${aws_security_group.private.id}"]
+
+
+  # Auto scaling group
+  asg_name                  = "mysql-asg"
+  vpc_zone_identifier       = ["${aws_subnet.private.id}"]
+  health_check_type         = "EC2"
+  min_size                  = 3
+  max_size                  = 6
+  desired_capacity          = 3
+  wait_for_capacity_timeout = 0
+  tags_as_map = {
+
+      Name = "${var.Name}-MySQL"
+      Env = "${var.Env}"
+      Created_by = "${var.Created_by}"
+      Dept = "${var.Dept}"
+  }
+}
